8ac94b419dc52d065fb1092009706029fda734b4
Gagan Gorsi <gagangorsi251@gmail.com>
2026-02-04 18:26:39 +0000

    suppose user forget the password then after click on the forget password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -105,7 +105,7 @@ export default function LoginPage() {
                 <div className="flex items-center justify-between">
                   <Label htmlFor="password">Password</Label>
                   <Link
-                    href="#"
+                    href="/forgot-password"
                     className="text-sm font-medium text-primary underline-offset-4 hover:underline"
                   >
                     Forgot Password?
