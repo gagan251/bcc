13d2095c485d6d5b4ecab1f3cb20c2bacfd3e2a5
Gagan Gorsi <gagangorsi251@gmail.com>
2026-02-06 06:39:25 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { FirebaseClientProvider } from '@/firebase';
 import { ThemeProvider } from '@/components/theme-provider';
-import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -27,17 +26,6 @@ export default function RootLayout({
           inter.variable
         )}
       >
-        <NextTopLoader
-          color="#3b82f6"
-          initialPosition={0.08}
-          crawlSpeed={200}
-          height={3}
-          crawl={true}
-          showSpinner={false}
-          easing="ease"
-          speed={200}
-          shadow="0 0 10px #3b82f6, 0 0 5px #3b82f6"
-        />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
